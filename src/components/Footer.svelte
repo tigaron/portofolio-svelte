3e81a1fd02d0616efb974862ec193c915bc019a1
tigaron <suputra@mail.com>
2024-07-03 22:26:23 +0700

    fix: change anchor to button

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,15 +4,15 @@
 	<p class="px-4 py-2 bg-white text-slate-950 font-medium">Connect with me &darr;</p>
 	<div class="flex flex-col gap-4 items-center justify-center">
 		<div class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto">
-			<a href="mailto:suputra.iwn@gmail.com" target="_blank">
+			<button on:click={() => window.open('mailto:suputra.iwn@gmail.com', '_blank')}>
 				<i class="fa-regular fa-envelope" />
-			</a>
-			<a href="https://www.linkedin.com/in/nikesuputra/" target="_blank">
+			</button>
+			<button on:click={() => window.open('https://www.linkedin.com/in/nikesuputra/', '_blank')}>
 				<i class="fa-brands fa-linkedin-in" />
-			</a>
-			<a href="https://github.com/tigaron/" target="_blank">
+			</button>
+			<button on:click={() => window.open('https://github.com/tigaron/', '_blank')}>
 				<i class="fa-brands fa-github" />
-			</a>
+			</button>
 		</div>
 	</div>
 </footer>
